32bb728300eb7bdbae070317a3051b0195dc9b5b
lbarco248-glitch <lbarco248@gmail.com>
2026-04-09 13:35:00 -0500

    First Commit

---
+++ src/main/java/com/projectmoonfan/Main.java
@@ -0,0 +1,7 @@
+package com.projectmoonfan;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    }
---
+++ src/main/java/com/projectmoonfan/variableApp.java
@@ -0,0 +1,17 @@
+package com.projectmoonfan;
+
+public class variableApp {public static void main(String[] args)
+{
+    String favorite_color = "Orange";
+    short year_start = 2026;
+    char middle_initial = 'A';
+    boolean pets = true;
+    String catchphrase = "The end is nigh!";
+    System.out.println("My favorite color is " + favorite_color);
+    System.out.println("I started coding in " + year_start);
+    System.out.println("My middle initial is " + middle_initial);
+    System.out.println("And it is " + pets + " that I have pets.");
+    System.out.println(catchphrase);
+
+}
+}
