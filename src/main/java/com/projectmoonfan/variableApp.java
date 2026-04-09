2bf3f20725fb793ff5f1230a326659c9b5e3e4ed
lbarco248-glitch <lbarco248@gmail.com>
2026-04-09 13:55:10 -0500

    Include completed Exercise 2 and organized code

--- src/main/java/com/projectmoonfan/variableApp.java
+++ src/main/java/com/projectmoonfan/variableApp.java
@@ -2,16 +2,28 @@ package com.projectmoonfan;
 
 public class variableApp {public static void main(String[] args)
 {
+    // Exercise 1
     String favorite_color = "Orange";
+        System.out.println("My favorite color is " + favorite_color);
     short year_start = 2026;
+        System.out.println("I started coding in " + year_start);
     char middle_initial = 'A';
+        System.out.println("My middle initial is " + middle_initial);
     boolean pets = true;
+        System.out.println("And it is " + pets + " that I have pets.");
     String catchphrase = "The end is nigh!";
-    System.out.println("My favorite color is " + favorite_color);
-    System.out.println("I started coding in " + year_start);
-    System.out.println("My middle initial is " + middle_initial);
-    System.out.println("And it is " + pets + " that I have pets.");
-    System.out.println(catchphrase);
+        System.out.println(catchphrase);
+
+
+    //Exercise 2
+    int week_days = 7;
+        System.out.println("There are " + week_days + " days of the week.");
+    float coffee_price = 4.99F;
+        System.out.println("The price of coffee is " + coffee_price+ ".");
+    char favorite_letter = 'W';
+        System.out.println("My favorite letter is " + favorite_letter + ".");
+    boolean rain = false;
+        System.out.println("It would be " + rain + " to say it is raining.");
 
 }
 }
